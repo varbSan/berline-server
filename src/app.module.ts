e90e67e05c290aeae42ad768cdd5c1af21ee9795
Bakary <var.bsan@gmail.com>
2025-02-23 15:21:00 +0100

    fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './user/user.module';
 import { QueuePointModule } from './queuePoint/queuePoint.module';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
 
-const isDeveloppement = process.env.NODE_ENV === 'development';
+dotenv.config();
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: false, // Enable playground
-      autoSchemaFile: 'schema.gql', // Specify the path for the schema file
-      debug: isDeveloppement,
-      introspection: isDeveloppement,
+      playground: false, // no need. use altair graphql client instead
+      autoSchemaFile: 'schema.gql',
+      debug: process.env.NODE_ENV === 'development',
+      introspection: process.env.NODE_ENV === 'development',
     }),
     UserModule,
     AuthModule,
