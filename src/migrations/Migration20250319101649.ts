958d6ebbddc5e9df284c33be94eb50682747bf1e
Bakary <var.bsan@gmail.com>
2025-03-19 11:27:49 +0100

    fix: reimplement migrations

---
+++ src/migrations/Migration20250319101649.ts
@@ -0,0 +1,13 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250319101649 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`create table "queue_point" ("id" serial primary key, "row" int not null, "created_at" timestamptz not null);`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`drop table if exists "queue_point" cascade;`);
+  }
+
+}
